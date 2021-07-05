ac557f830ada5197c438826ede57d16346bb1493
minwei123 <mk19952109494>
2021-07-05 23:55:22 +0800

    添加一个自定义的控制文件。可以在swager ui 中显示出来

---
+++ src/SmartAbp.HttpApi/Controllers/HooksController.cs
@@ -0,0 +1,20 @@
+﻿using SmartAbp.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+using SmartAbp.Books;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using SmartAbp.Controllers;
+
+namespace SmartAbp.HooksController
+{
+    [Route("api/Hooks")]
+    public class HooksController : SmartAbpController
+    {
+
+        [HttpGet("crea")]
+        public string Crea()
+        {
+            return "sadavdgasvdgafs";
+        }
+    }
+}
