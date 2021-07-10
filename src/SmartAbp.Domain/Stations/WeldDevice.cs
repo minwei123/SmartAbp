e74eca9168e68e6acd9a8b9225ca8b00bf0a90c1
Dashark <dasha.hu@gmail.com>
2021-07-10 14:08:29 +0800

    大致实现了工作站模型

--- src/SmartAbp.Domain/Stations/WeldDevice.cs
+++ src/SmartAbp.Domain/Stations/WeldDevice.cs
@@ -4,26 +4,38 @@ namespace SmartAbp.Stations
 {
   public class WeldSection : ValueObject
   {
-    public int id { get; set; }
-    public float voltage { get; set; }
-    public float current { get; set; }
-    public float speed { get; set; }
+    public int id { get; private set; }
+    public float voltage { get; private set; }
+    public float current { get; private set; }
+    public float speed { get; private set; }
+    private WeldSection() { }
+    public WeldSection(int id, float vol, float cur, float speed) {
+      this.id = id;
+      voltage = vol;
+      current = cur;
+      this.speed = speed;
+    }
+  }
+  public enum RobotType {
+    WeldingRobot,
+    TransferRobot
   }
   public class Robot : Entity<Guid>
   {
-    public List<WeldSection> WeldSections { get; set; }
+    public RobotType robotType { get; private set; }
+    public List<WeldSection> weldSections { get; set; }
       protected Robot() {
 
       }
       // 实体的主键在基类（Entity<Guid>）中定义了，Robot没有再定义
-      public Robot(Guid id) : base(id) {
-
+      public Robot(Guid id, RobotType rt) : base(id) {
+        robotType = rt;
       }
     public void maintain() {
-
+      // 对机器人的保养
     }
     public void repaire() {
-
+      // 对机器人的维修
     }
     public void welding(int id, float volt, float curr, float speed) {
 
