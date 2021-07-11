f6597dbc579f762ad7e4003515dc7bee1db9d000
minwei123 <mk19952109494>
2021-07-12 00:50:12 +0800

    工作站的服务层

---
+++ src/SmartAbp.Application/Stations/StationAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace SmartAbp.Stations
+{
+    public class StationAppService:
+        CrudAppService<
+            Station, //The Book entity
+            StationDto, //Used to show books
+            Guid, //Primary key of the book entity
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateStationDto>, //Used to create/update a book
+        IStationAppService
+    {
+        public StationAppService(IRepository<Station, Guid> repository)
+            : base(repository)
+        {
+        }
+    }
+}
