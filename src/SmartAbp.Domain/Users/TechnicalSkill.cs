f8374cfcd44234c83b3e10f7a67ec8a7b72ef44e
Dashark <dasha.hu@gmail.com>
2021-07-17 23:18:31 +0800

    完善

--- src/SmartAbp.Domain/Users/TechnicalSkill.cs
+++ src/SmartAbp.Domain/Users/TechnicalSkill.cs
@@ -6,6 +6,6 @@ namespace SmartAbp.Users
 {
   // 技能接口，满足用户技能及设备技能要求匹配的需求。
   public interface ITechnicalSkill {
-    bool matchSkill(ITechnicalSkill other);
+    public bool matchSkill(ITechnicalSkill other);
   }
 }
